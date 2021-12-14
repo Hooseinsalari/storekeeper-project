4a4c361138e43946ce5a9a6e35c27ca56d0068d2
Hooseinsalari <hossein.salari002@gmail.com>
2021-12-14 14:15:22 +0330

    add toastify

--- my-app/src/components/AddProductComponent.js
+++ my-app/src/components/AddProductComponent.js
@@ -1,87 +1,136 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 
-import styles from "./AddProductComponent.module.css"
-import "../font/Mj_Ojan-Fontjo.com/Mj_Ojan.ttf"
+import styles from "./AddProductComponent.module.css";
+import "../font/Mj_Ojan-Fontjo.com/Mj_Ojan.ttf";
 
-const AddProductComponent = ({data, setData}) => {
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
+import { notify } from "./toastify";
+
+const AddProductComponent = ({ data, setData }) => {
   const [product, setProduct] = useState({
     name: "",
     category: "",
     quantity: "",
     description: "",
-    id: Math.floor(Math.random() *1000),
-    click: false    
-  })
+    id: Math.floor(Math.random() * 1000),
+    click: false,
+  });
 
   const [options, setOptions] = useState([
-    { value: "", label: "-- انتخاب --"},
+    { value: "", label: "-- انتخاب --" },
     { value: "لبنیات", label: "لبنیات" },
     { value: "خشکبار", label: "خشکبار" },
     { value: "تنقلات", label: "تنقلات" },
   ]);
 
-  const [newCategory, setNewCategory] = useState({value: "", label:""})
+  const [newCategory, setNewCategory] = useState({ value: "", label: "" });
 
   const inputHandler = (event) => {
-    setNewCategory({value:event.target.value, label: event.target.value})
-  }
+    setNewCategory({ value: event.target.value, label: event.target.value });
+  };
 
   const categoryHandler = (event) => {
-    console.log(newCategory)
-    setOptions([...options, newCategory])
-  }
-
+    console.log(newCategory);
+    setOptions([...options, newCategory]);
+  };
 
   const changeHandler = (event) => {
-    setProduct({...product, [event.target.name]: event.target.value})
-  }
+    setProduct({ ...product, [event.target.name]: event.target.value });
+  };
 
   const submitHandler = (event) => {
-    event.preventDefault()
-    setData([...data, product])
-    axios.post("http://localhost:3001/product", product)
-    setProduct({ name:"", category: "", quantity: 0})
-  }
-
+    event.preventDefault();
+    if(product.name && product.quantity && product.category) {
+      setData([...data, product]);
+      axios.post("http://localhost:3001/product", product);
+    } else {
+      notify("error", "اطلاعات اشتباه")
+      return
+    }
+    notify("success", "با موفقیت افزوده شد");
+    setProduct({ name: "", category: "", quantity: 0 });
+  };
   
+  // useEffect(() => {
+  //   if(data.length + 1) {
+  //     notify("success", "با موفقیت افزوده شد");
+  //   }
+  // },[])
 
   return (
     <div>
       <div className={styles.container}>
         <div className={styles.inputContainer}>
           <label className={styles.label}>افزودن دسته بندی</label>
-          <input className={styles.input} value={newCategory.value} onChange={inputHandler} />
-          <button className={styles.button} onClick={categoryHandler}>افزودن</button>
+          <input
+            className={styles.input}
+            value={newCategory.value}
+            onChange={inputHandler}
+          />
+          <button className={styles.button} onClick={categoryHandler}>
+            افزودن
+          </button>
         </div>
       </div>
-      <form onSubmit={submitHandler} className={styles.container} autoComplete='off'>
+      <form
+        onSubmit={submitHandler}
+        className={styles.container}
+        autoComplete="off"
+      >
         <div className={styles.inputContainer}>
           <label className={styles.label}>نام محصول</label>
-          <input className={styles.input} type="text" name="name" value={product.name} onChange={changeHandler} />
+          <input
+            className={styles.input}
+            type="text"
+            name="name"
+            value={product.name}
+            onChange={changeHandler}
+          />
         </div>
         <div className={styles.inputContainer}>
           <label className={styles.label}>دسته بندی</label>
-          <select onChange={changeHandler} name="category" className={`${styles.input} ${styles.select}`}>
-            {
-              options.map((option) => <option value={option.value} key={option.value}>{option.label}</option>)
-            }
+          <select
+            onChange={changeHandler}
+            name="category"
+            className={`${styles.input} ${styles.select}`}
+          >
+            {options.map((option) => (
+              <option value={option.value} key={option.value}>
+                {option.label}
+              </option>
+            ))}
           </select>
         </div>
         <div className={styles.inputContainer}>
           <label className={styles.label}>تعداد</label>
-          <input className={styles.input} type="number" name="quantity" value={product.quantity} onChange={changeHandler} />
+          <input
+            className={styles.input}
+            type="number"
+            name="quantity"
+            value={product.quantity}
+            onChange={changeHandler}
+          />
         </div>
         <div className={styles.inputContainer}>
           <label className={styles.label}>توضیحات</label>
-          <textarea className={`${styles.input} ${styles.textarea}`} name="description" value={product.description} onChange={changeHandler} />
+          <textarea
+            className={`${styles.input} ${styles.textarea}`}
+            name="description"
+            value={product.description}
+            onChange={changeHandler}
+          />
         </div>
         <div>
-          <button className={styles.button} type="submit">اضافه کردن محصول</button>
+          <button className={styles.button} type="submit">
+            اضافه کردن محصول
+          </button>
         </div>
       </form>
+      <ToastContainer />
     </div>
   );
 };
--- my-app/src/components/Products.js
+++ my-app/src/components/Products.js
@@ -3,14 +3,10 @@ import React, { useRef, useState } from "react";
 import styles from "./Products.module.css";
 
 // svg
-import arrowUp from "../svg/sort-up-solid.svg";
-import arrowDown from "../svg/sort-down-solid.svg";
 import axios from "axios";
 
 const Products = ({ data, setData }) => {
-
-
-    const details = useRef()
+  const details = useRef();
 
   const clickHandler = (id, e) => {
     // const index = data.findIndex((product) => product.id === id)
@@ -27,30 +23,47 @@ const Products = ({ data, setData }) => {
   };
 
   const deleteHandler = async (id) => {
-    await axios.delete(`http://localhost:3001/product/${id}`)
-    const {data} = await axios.get("http://localhost:3001/product")
-    setData(data)
-  }
- 
+    await axios.delete(`http://localhost:3001/product/${id}`);
+    const { data } = await axios.get("http://localhost:3001/product");
+    setData(data);
+  };
 
-  return (
+  return data.length ? (
     <div className={styles.container}>
       {data.map((product) => (
-        <details ref={details} key={product.id} className={styles.product} onClick={(e) => clickHandler(product.id, e)}>
+        <details
+          ref={details}
+          key={product.id}
+          className={styles.product}
+          onClick={(e) => clickHandler(product.id, e)}
+        >
           <summary className={styles.summary}>
             <p className={styles.proName}>{product.name}</p>
             <p className={styles.proCategory}>{product.category}</p>
             <p className={styles.proQuantity}>{product.quantity}</p>
-            <button className={styles.removeBtn} onClick={() => deleteHandler(product.id)}>حدف</button>
+            <button
+              className={styles.removeBtn}
+              onClick={() => deleteHandler(product.id)}
+            >
+              حدف
+            </button>
           </summary>
-          {
-            product.description ? 
-            <p className={styles.description}>{product.description}</p> :
-            <p>توضیحات وجود ندارد</p>
-          }
+          {product.description ? (
+            <div>
+              <p className={styles.description}>{product.description}</p>
+              <p>{Date()}</p>
+            </div>
+          ) : (
+            <div>
+              <p>توضیحات وجود ندارد</p>
+              <p>{Date()}</p>
+            </div>
+          )}
         </details>
       ))}
     </div>
+  ) : (
+    <h1>محصولی وجود ندارد</h1>
   );
 };
 
